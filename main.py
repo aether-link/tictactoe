9f279154443e5257b4a27fb8612adc10244be693
Sujan Sharma <138952788+NotSujanSharma@users.noreply.github.com>
2024-02-26 22:57:35 +0000

    board

---
+++ main.py
@@ -0,0 +1,48 @@
+# Marking the board
+def mark(h,c):
+    if(h | c):
+        if(h):
+            print(' O ', end='')
+        else:
+            print(' X ', end='')
+    else:
+        print('   ', end='')
+
+# Printing the board
+def display(human, computer):
+    p=1
+    for j in range(0, 3):
+        for i in range(0, 3):
+            print('|', end='')
+            mark(human[p], computer[p])
+            p+=1
+        print('|')
+
+
+if __name__ == '__main__':
+    # Dictionary to store the board records
+    human = {
+        1: True,
+        2: False,
+        3: False,
+        4: False,
+        5: False,
+        6: False,
+        7: False,
+        8: False,
+        9: False,
+
+    }
+    computer = {
+        1: False,
+        2: False,
+        3: True,
+        4: False,
+        5: False,
+        6: False,
+        7: False,
+        8: False,
+        9: False,
+
+    }
+    display(human, computer)
