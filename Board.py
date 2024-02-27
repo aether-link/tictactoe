21519e8f4b6964b7e56ff9a78a6b50a7b2551448
Sujan Sharma <ak@brokenai.ca>
2024-02-26 21:59:45 -0500

    working code - do not fucking touch

---
+++ Board.py
@@ -0,0 +1,29 @@
+from error import newError
+class Board:
+    def __init__(self):
+        self.board = dict()
+        self.set_empty()
+        
+    def set_empty(self):
+        self.board = {
+        1: " ",
+        2: " ",
+        3: " ",
+        4: " ",
+        5: " ",
+        6: " ",
+        7: " ",
+        8: " ",
+        9: " ",
+        }
+    
+    def update(self,userInput,player):
+        self.board[userInput] = "x"
+    
+    def get_data(self):
+        return dict(self.board)
+    
+    def checkPosition(self, userInput):
+        if self.board[userInput]=='X' or self.board[userInput]=='O':
+            raise newError("Position taken")
+    
--- player.py
+++ player.py
@@ -1,45 +1,29 @@
-class newError(Exception):
-    pass
+from Board import Board
+from error import newError
 
-class Player:
-    def __init__(self, computer):
-        self.human= {
-        1: True,
-        2: False,
-        3: False,
-        4: False,
-        5: False,
-        6: False,
-        7: False,
-        8: False,
-        9: False,
-        }
-        self.computer = computer
-        self.userInput = None
-    def get_positions(self):
-        return self.human
-
-    def get_user_input(self):
-        self.userInput = input("Enter your move:")
+class Player(Board):
+    def __init__(self, board_data):
+        self.board_data=board_data
+        self.player='x'
+        
+    def get_user_input(self, board):
+        userInput = input("Enter your move:")
         try:
-            self.userInput = int(self.userInput)
-            if self.userInput < 1 or self.userInput > 9:
+            userInput = int(userInput)
+            if userInput < 1 or userInput > 9:
                 raise newError("Value out of range")
             else:
-                self.checkPosition()
-                self.update()
-
+                board.checkPosition(userInput)
+                
+                return userInput
+                
         except ValueError:
             print("Invalid input")
-            self.get_user_input()
+            self.get_user_input(board)
 
         except newError as e:
             print(e)
-            self.get_user_input()
+            self.get_user_input(board)
               
-    def update(self):
-        self.human[self.userInput]=True
-    def checkPosition(self):
-        if self.human[self.userInput]==True or self.computer[self.userInput]==True:
-            raise newError("Position taken")
-        
+    
+
